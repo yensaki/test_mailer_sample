8b311da6b059c8f59ee3ca524cb1a60b780f40ae
saki <saki@pixiv.co.jp>
2017-07-29 01:23:41 +0900

    add mail

--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -3,4 +3,9 @@ class ReservationMailer < ApplicationMailer
     @reservation = reservation
     mail(to: 'dummy@example.com', subject: '【重要】予約が完了しました')
   end
+
+  def cancel_mail(reservation)
+    @reservation = reservation
+    mail(to: 'dummy@example.com', subject: '【お知らせ】予約をキャンセルしました')
+  end
 end
